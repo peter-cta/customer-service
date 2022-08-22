8cd6e22bcf4feaec5de5cfbcc83f67f551991ecd
peter-cta <peter.cta99@gmail.com>
2022-08-22 22:41:29 +0700

    Initial commit

---
+++ src/main/java/com/example/myproject2022/MyProject2022Application.java
@@ -0,0 +1,13 @@
+package com.example.myproject2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProject2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProject2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myproject2022/MyProject2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myproject2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProject2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
